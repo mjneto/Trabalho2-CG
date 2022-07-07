29ab947bd65ef0e0c719079dea96b71fdc1d49d0
mjneto <manoelneto.snts@gmail.com>
2022-07-07 01:38:34 -0300

    Adição de novo modelo e skybox
    * Esta é a versão que será usada na avaliação 3 de CG
    
    - Novo modelo 3D e animação
    - Geração do céu na cena
    - Limpeza do código
    - Atualização do README

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # Trabalho2-CG
+Construção de uma cena 3D aplicando conceitos de computação gráfica (iluminação, sombra, textura, animação, modelos 3D). Parte da avaliação da disciplina de CG do curso de SI - UFPA (2022.2)
 
 ### Feito com:
-- [Three.js](https://threejs.org/docs/index.html#manual/en)
+-  [`Three.js`](https://threejs.org/docs/index.html#manual/en)
 
 ### Utilização:
 O trabalho utiliza alguns modelos que são pesados, sendo recomendado executar localmente para obter melhor desempenho.
@@ -14,4 +15,9 @@ Versão no Codepen: [Trabalho2-CG](https://codepen.io/mjneto/pen/VwQqgEX)
 
 ### Créditos:
 - [Destroyed Warehouse In Kaarina, Finland](https://sketchfab.com/3d-models/destroyed-warehouse-in-kaarina-finland-12d2dfd3d2cb4c8e80f3f223c649be32) by [Lassi Kaukonen](https://sketchfab.com/thesidekick) licensed under [CC-BY-4.0](http://creativecommons.org/licenses/by/4.0/)
-- Vanguard by T.Choonyung from [mixamo.com](https://www.mixamo.com)
+- Vanguard by T.Choonyung and Kachujin G Rosales from [mixamo.com](https://www.mixamo.com)
+- Animations by [mixamo.com](https://www.mixamo.com)
+
+<sub>Por: Manoel Neto [^aluno]</sub>
+
+[^aluno]: Manoel José Santos Martins Neto - 201511140015 - Sistemas de Informação - UFPA
--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 import {OrbitControls} from "https://unpkg.com/three@0.142.0/examples/jsm/controls/OrbitControls.js"
 import {GLTFLoader} from "https://unpkg.com/three@0.142.0/examples/jsm/loaders/GLTFLoader.js"
 import {FBXLoader} from "https://unpkg.com/three@0.142.0/examples/jsm/loaders/FBXLoader.js"
+import {Sky} from "https://unpkg.com/three@0.142.0/examples/jsm/objects/Sky.js"
 import * as THREE from 'three';
 
 let clock = new THREE.Clock();
-let mixer;
+let mixer, mixer2;
+let sky, sun;
 
 //cena
 const scene = new THREE.Scene();
@@ -12,57 +14,67 @@ scene.backgroud = new THREE.Color(0xa0a0a0);
 scene.fog = new THREE.Fog(0xa0a0a0, 10, 50);
 
 //camera
-const camera = new THREE.PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  1000
-);
-camera.position.set(2.4763476326420695, 4.9727971392339225, 2.8385318466906226);
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.set(5, 5, 5);
 camera.lookAt(0, 1, 0);
 
 //renderizador
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setPixelRatio(window.devicePixelRatio);
+renderer.toneMapping = THREE.ACESFilmicToneMapping;
 renderer.shadowMap.enabled = true;
 
 document.body.appendChild(renderer.domElement);
 
 //controles
-const controls = new OrbitControls(camera, renderer.domElement);
+//const controls = new OrbitControls(camera, renderer.domElement);
 
 //luz ambiente
-const hemiLight = new THREE.HemisphereLight(0xffffff, 0x444444);
+const hemiLight = new THREE.HemisphereLight(0xd4c0aa, 0xb69b95, 0.5);
 hemiLight.position.set(0, 20, 0);
 scene.add(hemiLight);
 
 //luz direcional
-const dirLight = new THREE.DirectionalLight(0xffffff);
-dirLight.shadow.camera.left = -10;
-dirLight.shadow.camera.right = 10;
-dirLight.shadow.camera.top = 10;
+const dirLight = new THREE.DirectionalLight(0xffffff, 2);
+dirLight.shadow.camera.left = -20;
+dirLight.shadow.camera.right = 20;
+dirLight.shadow.camera.top = 20;
 
-dirLight.position.set(-10, 5, -20);
+dirLight.position.set(-10, 5, -25);
 dirLight.castShadow = true;
-dirLight.shadow.mapSize.width = 2048;
-dirLight.shadow.mapSize.height = 2048;
+dirLight.shadow.mapSize.width = 4096;
+dirLight.shadow.mapSize.height = 4096;
 scene.add(dirLight);
 
-var helper = new THREE.PointLightHelper(dirLight);
-scene.add(helper);
+//shader céu
+sky = new Sky();
+sky.scale.setScalar(10000);
+scene.add(sky);
 
-var helper2 = new THREE.CameraHelper(dirLight.shadow.camera);
-scene.add(helper2);
+const uniforms = sky.material.uniforms;
+uniforms["turbidity"].value = 10;
+uniforms["rayleigh"].value = 2;
+uniforms["mieCoefficient"].value = 0.005;
+uniforms["mieDirectionalG"].value = 0.8;
 
-//chão
-const mesh = new THREE.Mesh(
-  new THREE.PlaneGeometry(50, 30),
-  new THREE.MeshPhongMaterial({ color: 0x999999, depthWrite: false })
-);
-mesh.rotation.x = -Math.PI / 2;
-mesh.receiveShadow = true;
-scene.add(mesh);
+const parameters = {
+    elevation: 3,
+    azimuth: 200
+};
+
+const pmremGenerator = new THREE.PMREMGenerator( renderer );
+
+const phi = THREE.MathUtils.degToRad(90 - parameters.elevation);
+const theta = THREE.MathUtils.degToRad(parameters.azimuth);
+
+//"sol"
+sun = new THREE.Vector3();
+sun.setFromSphericalCoords(1, phi, theta);
+
+uniforms["sunPosition"].value.copy(sun);
+
+scene.environment = pmremGenerator.fromScene(sky).texture;
 
 //model_warehouse
 const loader = new GLTFLoader();
@@ -76,12 +88,6 @@ loader.load('./models/destroyed_warehouse/scene.gltf', (gltf1) => {
     gltf1.scene.scale.set(1, 1, 1);
     gltf1.scene.rotateY(-Math.PI / 2);
     scene.add(gltf1.scene);
-}
-, (error) => {
-    console.error(error);
-}
-, (progress) => {
-    console.log(progress);
 })
 
 //model_vanguard
@@ -108,17 +114,40 @@ loader2.load('vanguard.fbx', (fbx) => {
     scene.add(fbx);
 });
 
+//model_kachujin
+const loader3 = new FBXLoader();
+loader3.setPath('./models/kachuijin/');
+loader3.load('kachujin_g_rosales.fbx', (fbx) => {
+    fbx.scale.setScalar(0.01);
+    fbx.position.set(-7, 1.81, -5);
+    fbx.rotation.set(0, 1, 0);
+    fbx.traverse((child) => {
+        if (child.isMesh) {
+            child.castShadow = true;
+            child.receiveShadow = true;
+        }
+    });
+
+    const anim2 = new FBXLoader();
+    anim2.setPath('./models/kachuijin/');
+    anim2.load('boxing.fbx', (anim) => {
+        mixer2 = new THREE.AnimationMixer(fbx);
+        const box = mixer2.clipAction(anim.animations[0]);
+        box.play();
+    });
+    scene.add(fbx);
+});
+
 animate();
 
 function animate() {
   requestAnimationFrame(animate);
   
   const delta = clock.getDelta();
-  if (mixer) {
+  if (mixer && mixer2) {
     mixer.update(delta);
+    mixer2.update(delta);
   }
-
-  console.log(camera.position)
 
   renderer.render(scene, camera);
 }
